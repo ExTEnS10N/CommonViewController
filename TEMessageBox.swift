c08487fab5af0e24e1782f91d3f9fe6d01ad8caf
ExTEnS10N <sunx123x@126.com>
2016-03-09 19:42:33 +0800

    auto present multiple MSGBox in Queue
    if a msg box is presenting , the newer msgbox will be push into a
    queue, and wait until the presenting msgbox disappear.

--- TEMessageBox.swift
+++ TEMessageBox.swift
@@ -9,40 +9,49 @@ import UIKit
 
 class TEMessageBox{
 	
-	private static var _MessageBox:UIAlertController? = nil
+	private static var isPresenting = false
+		{
+		didSet{
+			if !isPresenting && MSGBoxQueue.count > 0{
+				let MSGTuple = MSGBoxQueue.removeFirst()
+				presentMessageBox(MSGTuple.0, parentVC: MSGTuple.1)
+			}
+		}
+	}
 	
-	private static func presentMessageBox(parentVC: UIViewController){
+	private static var MSGBoxQueue:[(UIAlertController, UIViewController)] = []
+	
+	private static func presentMessageBox(MSGBox:UIAlertController, parentVC: UIViewController){
+		isPresenting = true
 		if(parentVC.tabBarController != nil){
-			parentVC.tabBarController!.presentViewController(_MessageBox!, animated: true, completion: nil)
+			parentVC.tabBarController!.presentViewController(MSGBox, animated: true, completion: nil)
 		}
 		else if(parentVC.navigationController != nil){
-			parentVC.navigationController!.presentViewController(_MessageBox!, animated: true, completion: nil)
+			parentVC.navigationController!.presentViewController(MSGBox, animated: true, completion: nil)
 		}
 		else{
-			parentVC.presentViewController(_MessageBox!, animated: true, completion: nil)
+			parentVC.presentViewController(MSGBox, animated: true, completion: nil)
 		}
 	}
 	
 	///弹出OK消息对话框
 	static func OKBox(title: String, message: String, parentViewController: UIViewController, handler:(()->Void)? = nil)
 	{
-		if(_MessageBox == nil){
-			NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
-				_MessageBox = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
-				
-				// Action
-				let okAction = UIAlertAction(title: "好吧", style: UIAlertActionStyle.Default)
-					{(action: UIAlertAction!) -> Void in
-						_MessageBox = nil
-						if(handler != nil){handler!()}
-				}
-				_MessageBox!.addAction(okAction)
-				
-				//present
-				presentMessageBox(parentViewController)
-			})
-		}
-		else {print("Warning: Two more Message Boxes attempt to be presentted at the same time!")}
+		NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+			let _MessageBox = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+			
+			// Action
+			let okAction = UIAlertAction(title: "好吧", style: UIAlertActionStyle.Default)
+				{(action: UIAlertAction!) -> Void in
+					isPresenting = false
+					if(handler != nil){handler!()}
+			}
+			_MessageBox.addAction(okAction)
+			
+			//present
+			if isPresenting {MSGBoxQueue.append((_MessageBox, parentViewController))}
+			else {presentMessageBox(_MessageBox, parentVC: parentViewController)}
+		})
 	}
 	
 	///	弹出输入框
@@ -52,33 +61,31 @@ class TEMessageBox{
 			guard placeHolders.count == textFieldContent!.count
 				else {fatalError("Error: placeHolders.count != textFieldContent.count")}
 		}
-		if(_MessageBox == nil){
-			NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
-				_MessageBox = UIAlertController(title: title, message: nil, preferredStyle: UIAlertControllerStyle.Alert)
-				
-				// Action
-				let confirmAction = UIAlertAction(title: "确认", style: .Default, handler: { (_:UIAlertAction)	-> Void in
-					confirmHandler(_MessageBox!.textFields!)
-					_MessageBox = nil
-				})
-				let cancelAction = UIAlertAction(title: "取消", style: .Cancel, handler:{(_:UIAlertAction) -> Void in _MessageBox = nil
-				})
-				_MessageBox!.addAction(confirmAction)
-				_MessageBox!.addAction(cancelAction)
-				
-				// textField
-				for var i = 0; i < placeHolders.count; ++i{
-					_MessageBox?.addTextFieldWithConfigurationHandler({ (textField:UITextField) -> Void in
-						textField.placeholder = placeHolders[i]
-						if textFieldContent != nil {textField.text = textFieldContent![i]}
-					})
-				}
-				
-				// present
-				presentMessageBox(parentVC)
+		NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+			let _MessageBox = UIAlertController(title: title, message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+			
+			// Action
+			let confirmAction = UIAlertAction(title: "确认", style: .Default, handler: { (_:UIAlertAction)	-> Void in
+				isPresenting = false
+				confirmHandler(_MessageBox.textFields!)
 			})
-		}
-		else {print("Warning: Two more Message Boxes attempt to be presentted at the same time!")}
+			let cancelAction = UIAlertAction(title: "取消", style: .Cancel, handler:{(_:UIAlertAction) -> Void in isPresenting = false
+			})
+			_MessageBox.addAction(confirmAction)
+			_MessageBox.addAction(cancelAction)
+			
+			// textField
+			for var i = 0; i < placeHolders.count; ++i{
+				_MessageBox.addTextFieldWithConfigurationHandler({ (textField:UITextField) -> Void in
+					textField.placeholder = placeHolders[i]
+					if textFieldContent != nil {textField.text = textFieldContent![i]}
+				})
+			}
+			
+			// present
+			if isPresenting {MSGBoxQueue.append((_MessageBox, parentVC))}
+			else {presentMessageBox(_MessageBox, parentVC: parentVC)}
+		})
 	}
 }
 
